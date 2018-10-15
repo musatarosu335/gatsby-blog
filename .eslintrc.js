6d524ffe4c69ccce44b15eeb8a57b5981a432705
Kentaro Sakae <musamusa0607@gmail.com>
2018-10-15 21:21:23 +0900

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "extends": [
+        "airbnb",
+        "prettier",
+        "prettier/react"
+    ],
+    plugins: ["prettier"],
+    "env": {
+        "browser": true
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "jsx-a11y/anchor-is-valid": ["error", {
+            "components": ["Link"],
+            "specialLink": ["to"]
+        }],
+        "prettier/prettier": [
+            "error",
+            {
+                trailingComma: "all",
+                singleQuote: true,
+                semi: true,
+                printWidth: 100,
+            },
+        ],
+        "react/forbid-prop-types": 0
+    }
+};
