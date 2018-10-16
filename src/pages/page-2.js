a07baa7d84669df7401bfb2985b7c667a6b55b4d
Kentaro Sakae <musamusa0607@gmail.com>
2018-10-16 23:04:41 +0900

    styled-components導入

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
+
+const ColoredTitle = styled.div`
+  color: red;
+  font-size: 20px;
+`;
 
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <ColoredTitle>Welcome to page 2</ColoredTitle>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 );
